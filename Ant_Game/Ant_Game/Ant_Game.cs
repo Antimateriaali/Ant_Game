e5a36c8d67b43deaa7f545fdd45cd66463b06c82
taaso <antti.liimatainen10@gmail.com>
2023-06-06 10:25:10 +0300

    Ant_Game push 1

---
+++ Ant_Game/Ant_Game/Ant_Game.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+namespace Ant_Game;
+
+public class Ant_Game : PhysicsGame
+{
+    public override void Begin()
+    {
+        // Kirjoita ohjelmakoodisi tähän
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+}

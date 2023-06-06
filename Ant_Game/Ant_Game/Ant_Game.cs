525d397faa1f624ba2a1204b1881d2e38f9c006b
taaso <antti.liimatainen10@gmail.com>
2023-06-06 11:34:45 +0300

    push

--- Ant_Game/Ant_Game/Ant_Game.cs
+++ Ant_Game/Ant_Game/Ant_Game.cs
@@ -9,11 +9,21 @@ namespace Ant_Game;
 
 public class Ant_Game : PhysicsGame
 {
+    Image StartAnt = LoadImage("MuurahainenSiivilla.png");
+    
     public override void Begin()
     {
-        // Kirjoita ohjelmakoodisi tähän
-
+        Kentta();
+        
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+
+    void Kentta()
+    {
+        PhysicsObject startant = new PhysicsObject(40, 40);
+        startant.Shape = Shape.Rectangle;
+        startant.Image = StartAnt;
+        Add(startant);
     }
 }

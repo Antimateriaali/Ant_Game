745132b6f50071c4b68df12b25411ad1341b54fb
taaso <antti.liimatainen10@gmail.com>
2023-06-07 11:26:15 +0300

    push

--- Ant_Game/Ant_Game/Ant_Game.cs
+++ Ant_Game/Ant_Game/Ant_Game.cs
@@ -14,9 +14,14 @@ public class Ant_Game : PhysicsGame
     Image StartAnt = LoadImage("Muurahainen.png");
     Image taustakuva = LoadImage("Maa.png");
     private Image Leaf = LoadImage("Lehti.png");
+    private Image Nest = LoadImage("Nest.png");
+    private Image[] Walk = LoadImages("Muurahainen.png", "Muurahainen1.png", "Muurahainen.png","Muurahainen2.png");
     private PhysicsObject startant;
+    
     public override void Begin()
     {
+        
+        
         Kentta();
 
         Camera.Follow(startant);
@@ -31,6 +36,10 @@ public class Ant_Game : PhysicsGame
         Keyboard.Listen(Key.W, ButtonState.Released, LiikutaPelaajaa, null, Vector.Zero);
         Keyboard.Listen(Key.S, ButtonState.Released, LiikutaPelaajaa, null, Vector.Zero);
 
+        Keyboard.Listen(Key.A, ButtonState.Pressed, Animatio, null);
+        Keyboard.Listen(Key.D, ButtonState.Pressed, Animatio, null);
+        Keyboard.Listen(Key.W, ButtonState.Pressed, Animatio, null);
+        Keyboard.Listen(Key.S, ButtonState.Pressed, Animatio, null);
         
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
@@ -38,41 +47,147 @@ public class Ant_Game : PhysicsGame
     
     void LiikutaPelaajaa(Vector vektori)
     {
-        
-        
+        // startant.Animation.Start();
+        // startant.Animation.Stop();
         startant.Velocity = vektori;
         startant.MaxVelocity = 200;
 
         if (vektori.Y < 0)
         {
+            
+            startant.MaxVelocity = 200;
             startant.Angle = Angle.StraightAngle;
         }
         else if (vektori.Y > 0)
         {
+            
+            startant.MaxVelocity = 200;
             startant.Angle = Angle.Zero;
         }
         else if (vektori.X < 0)
         {
+            
+            startant.MaxVelocity = 200;
             startant.Angle = Angle.RightAngle;
         }
         else if (vektori.X > 0)
         {
+            
+            startant.MaxVelocity = 200;
             startant.Angle = Angle.FromDegrees(-90);
         }
+        else
+        {
+            startant.Animation.Stop();
+            startant.MaxVelocity = 0;
+        }
+    }
+
+    void Animatio()
+    {
+        startant.Animation.Start();
     }
     
     void Kentta()
     {
         Level.Background.Image = taustakuva;
+        int kentankoko = 4000;
+        Level.Width = kentankoko;
+        Level.Height = kentankoko;
+        Level.CreateBorders();
+        Level.Background.FitToLevel();
+        Camera.StayInLevel = true;
         
         startant = new PhysicsObject(40, 40);
-        startant.Shape = Shape.Rectangle;
-        startant.Image = StartAnt;
-        Add(startant);
+        //Shape antmuoto = Shape.FromImage(StartAnt);
+        startant.Shape = Shape.Hexagon;
+        //startant.Image = StartAnt;
+        startant.Animation = new Animation(Walk);
         
-        PhysicsObject leaf = new PhysicsObject(40, 40);
-        leaf.Shape = Shape.Rectangle;
-        leaf.Image = Leaf;
-        Add(leaf);
+        
+        
+        startant.Animation.FPS = 3;
+        startant.CanRotate = false;
+        Add(startant);
+
+        for (int o = 0; o < 5; o++)
+        {
+            PhysicsObject leaf = new PhysicsObject(200, 200);
+            
+            leaf.Shape = Shape.Rectangle;
+            leaf.Image = Leaf;
+            leaf.CanRotate = false;
+            leaf.MaxVelocity = 0;
+            leaf.IgnoresCollisionResponse = true;
+            leaf.Y = RandomGen.NextDouble(-2000, 2000);
+            leaf.X = RandomGen.NextDouble(-2000, 2000);
+            
+            Add(leaf, -1);
+        }
+        
+        
+        PhysicsObject pesa = new PhysicsObject(80, 80);
+        pesa.Shape = Shape.Rectangle;
+        pesa.Image = Nest;
+        pesa.CanRotate = false;
+        pesa.MaxVelocity = 0;
+        pesa.IgnoresCollisionResponse = true;
+        
+        Add(pesa,-1);
+
+        for (int i = 0; i < 100; i++)
+        {
+            PhysicsObject ant = new PhysicsObject(40, 40);
+            
+            ant.Shape = Shape.Hexagon;
+            ant.Animation = new Animation(Walk);
+            ant.Animation.Start();
+            ant.Image = StartAnt;
+            
+            ant.CanRotate = false;
+            ant.Angle = Angle.FromDegrees(-90);
+            RandomMoverBrain satunnaisaivot = new RandomMoverBrain(200);
+            satunnaisaivot.TurnWhileMoving = true;
+            satunnaisaivot.WanderRadius = 500;
+            
+            
+            FollowerBrain aivot = new FollowerBrain("leaf");
+            aivot.Speed = 100;                 
+            aivot.DistanceFar = 60000;           
+            aivot.DistanceClose = 10;         
+            aivot.StopWhenTargetClose = false;  
+            //aivot.FarBrain = aivot;
+            //aivot.CloseBrain = satunnaisaivot;
+            
+            ant.Brain = satunnaisaivot;
+            ant.MaxVelocity = 200;
+            Add(ant);
+        }
+        
+        /*for (int i = 0; i < 100; i++)
+        {
+            PhysicsObject ant1 = new PhysicsObject(40, 40);
+            
+            ant1.Shape = Shape.Hexagon;
+            ant1.Animation = new Animation(Walk);
+            
+            ant1.Animation.Start();
+            
+            ant1.Image = StartAnt;
+            ant1.Color = new Color(255, 0, 0);
+            ant1.CanRotate = false;
+            RandomMoverBrain satunnaisaivot = new RandomMoverBrain(200);
+            FollowerBrain aivot = new FollowerBrain("leaf");
+            aivot.Speed = 300;                 // Millä nopeudella kohdetta seurataan
+            aivot.DistanceFar = 600;           // Etäisyys jolla aletaan seurata kohdetta
+            aivot.DistanceClose = 200;         // Etäisyys jolloin ollaan lähellä kohdetta
+            aivot.StopWhenTargetClose = true;  // Pysähdytään kun ollaan lähellä kohdetta
+            aivot.FarBrain = satunnaisaivot;
+            
+            ant1.Brain = aivot;
+            ant1.MaxVelocity = 200;
+            Add(ant1);
+        }*/
+        
     }
 }
